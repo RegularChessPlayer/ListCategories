95ec2f262c60f0797f20f50d2c071acc5785be86
Thiago Rodigues <thiago.engh@gmail.com>
2018-07-21 22:41:23 -0300

    Add ItemMenuEdit

--- catalog/application.py
+++ catalog/application.py
@@ -13,33 +13,54 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/category/<int:category_id>')
-def menu(category_id):
-    category = session.query(Category).filter_by(id=category_id).one()
-    items = session.query(CategoryItem).filter_by(category_id=category.id)
-    return render_template('menu.html', category=category, items=items)
+@app.route('/category/<string:category_name>')
+def menu(category_name):
+    if(not category_name.strip()):
+        return "Hello all categories"
+    else:
+        category = session.query(Category).filter_by(name=category_name).one()
+        items = session.query(CategoryItem).filter_by(category_id=category.id)
+        return render_template('menu.html', category=category, items=items)
 
 
-@app.route('/categoryItem/<int:category_id>/new', methods=['GET', 'POST'])
-def newCategoryItem(category_id):
+@app.route('/categoryItem/<string:category_name>/new', methods=['GET', 'POST'])
+def newCategoryItem(category_name):
     if request.method == 'POST':
+        category = session.query(Category).filter_by(name=category_name).one()
         newCategoryItem = CategoryItem(name=request.form['name'],
                                        description=request.form['description'],
-                                       category_id=category_id)
+                                       category_id=category.id)
         session.add(newCategoryItem)
         session.commit()
-        return redirect(url_for('menu', category_id=category_id))
+        return redirect(url_for('menu', category_name=category_name))
     else:
-        return render_template('newcategoryitem.html', category_id=category_id)
+        return render_template('newcategoryitem.html',
+                               category_name=category_name)
 
 
-@app.route('/category/<int:category_id>/<int:category_item_id>/edit')
-def editMenuItem(category_id, category_item_id):
-    return "Page to edit a new categoryId."
+@app.route('/category/<string:category_name>/<string:category_item_name>/edit',
+           methods=['GET', 'POST'])
+def editCategoryItem(category_name, category_item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    categoryItem = session.query(CategoryItem).filter_by(
+                                                category_id=category.id,
+                                                name=category_item_name).one()
+    if request.method == 'POST':
+        categoryItem.name = request.form['name']
+        categoryItem.description = request.form['description']
+        session.add(categoryItem)
+        session.commit()
+        return redirect(url_for('menu', category_name=category_name))
+    else:
+        return render_template('editcategoryitem.html',
+                               category_name=category_name,
+                               category_item_name=category_item_name,
+                               item=categoryItem)
 
 
-@app.route('/category/<int:category_id>/<int:category_item_id>/delete')
-def deleteMenuItem(category_id, category_item_id):
+@app.route('/category/<string:category_name>/<string:category_item_name>'
+           '/delete', methods=['GET', 'POST'])
+def deleteMenuItem(category_name, category_item_name):
     return "Page to delete menu item."
 
 
